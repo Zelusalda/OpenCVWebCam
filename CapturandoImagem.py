1e1f138ea1f983566a87c931f77a32e7fa16abbd
José Saldanha <101759742+Zelusalda@users.noreply.github.com>
2025-12-06 19:08:35 -0300

    Update CapturandoImagem.py
    Adicionando meio de captura de rosto

--- CapturandoImagem.py
+++ CapturandoImagem.py
@@ -1,13 +1,21 @@
 import cv2
 
 webCam = cv2.VideoCapture(0)
+classificadorFace = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 while True:
     camera, frame = webCam.read()
 
-    cv2.imshow("Imagem da WebCam", frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detec = classificadorFace.detectMultiScale(gray)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+    for(x, y, w, h) in detec:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.imshow("WebCam", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
 webCam.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindowns()
